431dfe54abb71ff8c1faa319c58eb261f06b3f38
Johan Bové <johan.bove@gmail.com>
2019-12-16 22:56:38 +0100

    Changes publish script Moved the json files to the now/data folder

--- scripts/taskwarrior_publish.sh
+++ scripts/taskwarrior_publish.sh
@@ -5,5 +5,5 @@ function task_export () {
     task end.after:today-mth project:Plans export > ~/task-completed.json
     theDate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     echo "{ \"updated\": \"${theDate}\" }" > ~/task-meta.json
-    scp -i $sshkey ~/task-active.json ~/task-ready.json ~/task-completed.json ~/task-meta.json $remote_login:$remote_private_html/now
+    scp -i $sshkey ~/task-active.json ~/task-ready.json ~/task-completed.json ~/task-meta.json $remote_login:$remote_private_html/now/data
 }
