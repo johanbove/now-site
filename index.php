5ccfeb39637875e4baf9d050ee85d4fe6b2f55ca
Johan Bové <johan.bove@gmail.com>
2019-12-18 00:04:13 +0100

    Refactored the app with Twig templates Work in progress

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
+$loader = new \Twig\Loader\FilesystemLoader('./views/');
+$twig = new \Twig\Environment($loader, [
+  'debug' => true,
+  'cache' => './compilation_cache',
+  'strict_variables' => true,
+  'autoescape' => false
+]);
+
+// Application logic
 require_once __DIR__ . "/now_code.inc";
-require_once __DIR__ . "/app.php";
+
+$template = $twig->load('home.twig');
+
+echo $template->render([
+  'lang' => 'en',
+  'classNames' => [
+    'html' => 'h-feed',
+    'title' => 'p-name',
+  ],
+  'version' => 'v1.4.4',
+  'meta' => get_meta(),
+  'data_active' => tasks_active(),
+  'data_ready' => tasks_ready(),
+  'data_completed' => tasks_completed(),
+]);
