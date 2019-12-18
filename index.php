5ae6d2a21594be149e58fc3c99e0bcf8d065ef17
Johan Bové <johan.bove@gmail.com>
2019-12-18 22:47:35 +0100

    Bump version

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ echo $template->render([
     'html' => 'h-feed',
     'title' => 'p-name',
   ],
-  'version' => 'v1.4.4',
+  'version' => 'v1.5.0',
   'meta' => get_meta(),
   'data_active' => tasks_active(),
   'data_ready' => tasks_ready(),
