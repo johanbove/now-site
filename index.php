285f5fe8c0a4cdd2a3b25fcf3208fc7123e9e7f5
Johan Bové <--global>
2019-12-24 13:46:48 +0100

    Bump version

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ echo $template->render([
     'html' => 'h-feed',
     'title' => 'p-name',
   ],
-  'version' => 'v1.6.2',
+  'version' => 'v1.7.0',
   'meta' => get_meta(),
   'data_active' => tasks_active(),
   'data_ready' => tasks_ready(),
